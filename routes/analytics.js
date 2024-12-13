6a2861ba5ca5c87f3f8b2dbcc5d132117a8d0a66
Maha <mmahaece07@gmail.com>
2024-12-13 17:22:20 +0530

    merging issue  resolved

--- routes/analytics.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAnalytics } = require("../controllers/analytics");
-const wrapAsync = require("../middleware/wrapAsync");
-const { authorization } = require("../middleware/authorization");
-
-router.get("/", authorization, wrapAsync(getAnalytics));
-
-module.exports = router;
